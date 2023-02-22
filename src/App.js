9e003ef160c6c6ae0dc470582c6b35b8ba0cfac8
VazManYan <vazgenar.1993@gmail.com>
2023-02-22 15:33:09 +0200

    cat-wine-shop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import './Reset.css';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Story from './Story';
+import Visit from './Visit';
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+        <Header/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element = {<Home/>}/>
+            <Route path="/story" element = {<Story/>}/>
+            <Route path="/visit" element= {<Visit/>}/>
+          </Routes>
+        </BrowserRouter>
+
+
+
     </div>
   );
 }
